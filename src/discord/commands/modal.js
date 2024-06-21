e712d4076d3f90ac50f8cf5f82834590032f2f07
D3vEduardo <eduardoaugustolimabueno423@gmail.com>
2024-06-21 15:31:03 -0300

    add the snippets

--- src/discord/commands/modal.js
+++ src/discord/commands/modal.js
@@ -1,35 +1,37 @@
-const { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, ComponentType } = require("discord.js");
-const { ReplyModal } = require("@base");
-
+const { SlashCommandBuilder, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle } = require("discord.js");
+const { ReplyModal } = require("@base")
 module.exports = {
   data: new SlashCommandBuilder()
-    .setDMPermission(true)
     .setName("modal")
-    .setDescription("Mostra um modal."),
+    .setDescription("Envia um modal.")
+    .setDMPermission(true),
   execute: async (interaction) => {
     const Modal = new ModalBuilder()
-    .setCustomId("Modal")
-    .setTitle("The Modal")
-    .setComponents([
+      .setCustomId("Modal")
+      .setTitle("The Modal")
+      .setComponents([
         new ActionRowBuilder().addComponents([
-            new TextInputBuilder()
+          new TextInputBuilder()
             .setCustomId("Input 01")
             .setStyle(TextInputStyle.Short)
             .setLabel("Informe algo no campo abaixo:")
             .setPlaceholder("Informe algo aqui!")
-            .setRequired(true)
-        ])
-    ])
+            .setRequired(true),
+        ]),
+      ]);
 
-    await interaction.showModal(Modal)
+    await interaction.showModal(Modal);
 
-    ReplyModal({
+    ReplyModal(
+      {
         customId: "Modal",
-        run: async ModalInteraction => {
-            const InputValue = ModalInteraction.fields.getTextInputValue("Input 01");
-            await ModalInteraction.reply({ content: InputValue });
-        }
-    }, interaction)
-
+        run: async (ModalInteraction) => {
+          const InputValue =
+            ModalInteraction.fields.getTextInputValue("Input 01");
+          await ModalInteraction.reply({ content: InputValue });
+        },
+      },
+      interaction
+    );
   },
 };
